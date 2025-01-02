56ba6604adf004413aebc07b07808e4a9e8e20c1
Max Sveshnikov <msveshnikov@gmail.com>
2025-01-02 21:16:08 +0100

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ClubManagement from './ClubManagement';
 import Profile from './Profile';
 import './i18n.jsx';
 import { I18nextProvider } from 'react-i18next';
+import ReactGA from 'react-ga4';
 
 export const API_URL = import.meta.env.DEV ? 'http://localhost:3000' : 'https://aicoachpro.ai';
 export const AuthContext = createContext(null);
@@ -16,6 +17,11 @@ function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [userProfile, setUserProfile] = useState(null);
     const toast = useToast();
+    
+    useEffect(() => {
+        ReactGA.initialize('G-01VHJVVKF3');
+        ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+    }, []);
 
     useEffect(() => {
         // const checkAuth = async () => {
